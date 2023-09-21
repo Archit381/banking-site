1f3a5cba3430bccc041eebf31b695345c4590574
Archit <architojha420@gmail.com>
2023-09-22 04:41:44 +0530

    completed carddeal component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import styles from '../style'
 
 const Button = ( ) => {
   return (
-    <button type='button' className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none mt-10`}>Get Started</button>
+    <button type='button' className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none mt-10 rounded-[10px]`}>Get Started</button>
   )
 }
 
--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import styles, { layout } from "../style";
+import Button from "./Button";
+import { card } from "../assets";
 
 const CardDeal = () => {
   return (
-    <div>CardDeal</div>
-  )
-}
+    <section className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h7 className={styles.heading2}>
+          Find a better card deal <br className="sm:block hidden" /> in few easy
+          steps.
+        </h7>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Arcu tortor, purus in mattis at sed integer faucibus. Aliquet quis
+          aliquet eget mauris tortor.c Aliquet ultrices ac, ametau.
+        </p>
 
-export default CardDeal
+        <Button styles="mt-10" />
+      </div>
+      <div className={layout.sectionImg}>
+        <img src={card} alt="card_img" className="w-[100%] h-[100%]" />
+      </div>
+    </section>
+  );
+};
+
+export default CardDeal;
